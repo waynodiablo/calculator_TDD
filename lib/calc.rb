1f4a10433eca79b0580ea55b102062e8dec70f3e
Wayne Stevens <wayne.stevens1@digital.homeoffice.gov.uk>
2018-10-31 14:03:51 +0000

    User stories 1 and 2 done. Stuck on 3

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,7 +1,19 @@
 class Calculator
   def multiply(a,b)
-    a * b
+    x = a * b
+    display(x)
   end
 
+  def square(a)
+    x = multiply(a,a)
+    display(x)
+  end
+
+def display(x)
+  "the answer is #{x}"
+end
 
 end
+
+# The code displays two lots of outputs
+# if choose to square.
--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -10,4 +10,14 @@ describe Calculator do
     expect(subject.multiply(1,2)).to eq 2
   end
 
+  it 'squares a number' do
+    expect(subject.square(3)).to eq 9
+  end
+
+  it 'outputs in the format "the answer is xxxxxx"' do
+    expect(stdout).to include("the answer is")
+  end
+
 end
+
+# Unsure about testing for an output string
